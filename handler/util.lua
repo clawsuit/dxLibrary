ea498091a13690830ef77623ebc6f5242e05dfa2
ClawSuit <43434260+clawsuit@users.noreply.github.com>
2022-05-10 22:28:15 +0200

    Version v1.0 Full

--- handler/util.lua
+++ handler/util.lua
@@ -9,6 +9,7 @@ ElementTypeChildrenAvailable = {['dxWindow'] = true}
 filesAvailables = {
     {'Basic-Regular.ttf', {10, false}, {11, true}},
     {'letterbold.otf', {9, true}, {10, true}},
+    {'Comforta_regular.ttf', {10, false}, {11, false}, {12, false}, {13, false}},
     --'boton.png',
 }
 
