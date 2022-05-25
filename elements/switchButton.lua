4106ccdc82b459df67b3995781f773951ecbc92f
ClawSuit <43434260+clawsuit@users.noreply.github.com>
2022-05-24 23:45:14 -0400

    Add files via upload

---
+++ elements/switchButton.lua
@@ -0,0 +1,62 @@
+function dxSwitchButton(x, y, w, h, parent)
+	
+	local self, element = createElement( 'dxSwitchButton', parent, sourceResource )
+	if self then
+		
+		self.x = math.round(x)
+		self.y = math.round(y)
+		self.w = math.min(math.round(w), math.round(53*sw))
+		self.h = math.min(math.round(h), math.round(23*sh))
+		self.parent = parent
+
+		self.colorbackground = tocolor(255, 255, 255)
+		self.colorOn = tocolor(120, 95, 205)
+		self.colorOff = tocolor(255, 0, 0)
+		
+		if self.parent then
+			self.offsetX = self.x - Cache[self.parent].x
+        	self.offsetY = self.y - Cache[self.parent].y
+        end
+
+        self.bx = 0
+        self.state = false
+      	self.update = true
+        return element
+	
+	end
+end
+
+function dxSwitchButtonSetState(element, bool)
+	local self = Cache[element]
+	if self then
+		self.state = bool
+		return true
+	end
+	return false
+end
+
+function dxSwitchButtonGetState(element)
+	local self = Cache[element]
+	if self then
+		return self.state
+	end
+	return false
+end
+
+function dxSwitchButtonSetColorOn(element, r, g, b, a)
+	local self = Cache[element]
+	if self then
+		self.colorOn = tocolor(r, g, b, a)
+		return true
+	end
+	return false
+end
+
+function dxSwitchButtonSetColorOff(element, r, g, b, a)
+	local self = Cache[element]
+	if self then
+		self.colorOff = tocolor(r, g, b, a)
+		return true
+	end
+	return false
+end
