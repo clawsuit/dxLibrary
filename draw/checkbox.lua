7a7488cad2728cb2af204b1a286070fb7098bf8f
Alex Leon <43434260+clawsuit@users.noreply.github.com>
2023-08-09 13:31:55 -0400

    v1.5 fix
    v1.5 fix

--- draw/checkbox.lua
+++ draw/checkbox.lua
@@ -23,55 +23,28 @@ function Render.dxCheckBox( element, parent, offX, offY )
 		x2, y2 = x2 + (offX or 0), y2 + (offY or 0)
 
 		if getKeyState( 'mouse1' ) and not self.click and not self.isDisabled then
-		
 			if isCursorOver(x2, y2, self.w, self.h) then
-
-				self.state = not self.state
-				self.text = self.state and checkBoxTypes[self.style] or ''
-			 	triggerEvent('onClick', element)
-				self.update = true
-				
-			end
-
-		end
-
-		if self.update or CLIENT_RESTORE then
-
-			if not isElement( self.rendertarget ) then
-				self.rendertarget = DxRenderTarget( self.w, self.h, true )
-			end
-
-			self.rendertarget:setAsTarget(true)
-			dxSetBlendMode( 'modulate_add' )
-			
-				if not self.svg then
-					DxDrawBorderedRectangle( 0, 0, self.w - 1, self.h - 1, self.colorbackground, self.colorborder, 1, false )
-				else
-					local alpha = bitExtract(self.colorbackground,24,8)
-					dxDrawImage(0, 0, self.w, self.h, self.svg, 0, 0, 0, tocolor( 255, 255, 255, alpha ))
+				if not self.isDisabled then
+					self.state = not self.state
+			 		triggerEvent('onClick', element)
 				end
-				dxDrawText( self.text, 0, 0, self.w, self.h, self.colortext, 1, self.font, 'center', 'center', true, true, false, false)
-
-			dxSetBlendMode("blend")
-			if isElement(parent) then
-				dxSetRenderTarget(Cache[parent].rendertarget)
-			else
-				dxSetRenderTarget()
 			end
-			self.update = nil
 		end
 
-		if isElement( self.rendertarget ) then
-			dxSetBlendMode("add")
-				dxDrawImage(x, y, self.w, self.h, self.rendertarget, 0, 0, 0, tocolor( 255, 255, 255, 255 ), false)
-			dxSetBlendMode("blend")
-		end
+		dxSetBlendMode("add")
+			local alpha = bitExtract(self.colorbackground,24,8)
+			dxDrawImage(x, y, self.w, self.h, self.svg, 0, 0, 0, tocolor( 255, 255, 255, alpha ))
+		dxSetBlendMode("blend")
+
+		dxSetBlendMode( 'modulate_add' )
+			if self.state then
+				dxDrawText2('✓', x, y, self.w, self.h, self.colorchecker, 1, self.font, 'center', 'center')
+			end
+
+			dxDrawText2(self.text, x+self.w+4, y, self.w, self.h-2, self.colortext, 1, self.font, 'left', 'center')
+		dxSetBlendMode("blend")
 		
-			
-		--dxDrawText( self.text, x, y, self.w+x, self.h+y, tocolor(120, 95, 205, 255), 1, Files['font']['Basic-Regular.ttf'][10], 'center', 'center', true, true, false, false)
 		self.click = getKeyState( 'mouse1' )
-
 	end
-
 end
 
